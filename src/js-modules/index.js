73728f72336f514d9e2bdb84f45a226081cb86af
Levente <eager-hun@users.noreply.github.com>
2017-12-25 06:48:24 +0100

    Fixed Vue.js mounting error on pages where the mount element is not present.

--- src/js-modules/index.js
+++ src/js-modules/index.js
@@ -4,7 +4,9 @@ console.log("webpack here goes!");
 import Vue from 'vue';
 import exampleComponent from "./vue-components/example-component.vue";
 
-const exampleVue = new Vue({
-    el: "#example-vue__mount",
-    render: h => h(exampleComponent)
-});
+if (document.getElementById("example-vue__mount")) {
+    new Vue({
+        el: "#example-vue__mount",
+        render: h => h(exampleComponent)
+    });
+}
